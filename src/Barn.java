160da50126506772850fa037a74ff917dedbda8c
EmilNordahn <60731699+EmilNordahn@users.noreply.github.com>
2020-03-20 10:03:34 +0100

    test

--- src/Barn.java
+++ src/Barn.java
@@ -1,2 +1,30 @@
 public class Barn {
+    private String fornavn;
+    private String efternavn;
+    private int cprnummer;
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEfternavn() {
+        return efternavn;
+    }
+
+    public void setEfternavn(String efternavn) {
+        this.efternavn = efternavn;
+    }
+
+    public int getCprnummer() {
+        return cprnummer;
+    }
+
+    public void setCprnummer(int cprnummer) {
+        this.cprnummer = cprnummer;
+    }
+
 }

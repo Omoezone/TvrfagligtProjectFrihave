669e147db45eedcc683cc561ece3ad2f52e35658
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-03-20 10:28:14 +0100

    Class
    Signed-off-by: Omoezone <30931803+Omoezone@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //Daniel
-        //TRALALALALA
-        // - Lukas
-
-        // HEJ - Wel
+        System.out.println("Hej");
     }
 }

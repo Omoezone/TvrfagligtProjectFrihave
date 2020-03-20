b84117455a0b209ecbcec6ddfcd3cde8b672e083
LukasHDyreborg <62420691+LukasHDyreborg@users.noreply.github.com>
2020-03-20 10:29:11 +0100

    Merge branch 'master' of C:\Users\Lukas\IdeaProjects\TvrfagligtProjectFrihave with conflicts.

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,7 @@
 public class Menu {
+    public static void menu() {
+        System.out.println("- Welcome to the menu -\n");
+        System.out.println("What would you like to do?\n");
+
+    }
 }

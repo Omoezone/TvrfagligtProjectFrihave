7a48c538f9545d8aad7ff8d55c0772152e38e820
Daniel <58211711+caerKea@users.noreply.github.com>
2020-03-20 10:02:24 +0100

    kontaktperson klasse og ingen stavekontrol

--- src/Kontaktperson.java
+++ src/Kontaktperson.java
@@ -1,2 +1,45 @@
 public class Kontaktperson {
+    private String fornavn;
+    private String efternavn;
+    private String telefon;
+    private String email;
+
+    public Kontaktperson(String fornavn, String efternavn, String telefon, String email) {
+        this.fornavn = fornavn;
+        this.efternavn = efternavn;
+        this.telefon = telefon;
+        this.email = email;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEfternavn() {
+        return efternavn;
+    }
+
+    public void setEfternavn(String efternavn) {
+        this.efternavn = efternavn;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

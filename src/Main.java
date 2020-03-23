bafe3ef1a34542717a54d701dfc1170c69bfb5fa
Omoezone <30931803+Omoezone@users.noreply.github.com>
2020-03-23 09:50:15 +0100

    Class
    Signed-off-by: Omoezone <30931803+Omoezone@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        Menu.menuSelection();
         Kontaktperson test = Kontaktperson.skabNyKontaktperson();
         System.out.println(test);
     }

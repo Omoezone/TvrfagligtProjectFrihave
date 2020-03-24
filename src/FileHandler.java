9c001c653fc66199dcb467c28decc858d4ee3d6b
LukasHDyreborg <62420691+LukasHDyreborg@users.noreply.github.com>
2020-03-24 10:38:22 +0100

    menu

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -13,24 +13,22 @@ public class FileHandler {
         }
     }
 
-    public static void loadFamilieToDisk(Familie familie) throws FileNotFoundException{
+    /*public static void loadFamilieToDisk(Familie familie) throws FileNotFoundException{
         PrintStream output = new PrintStream(new File("FamilieData.txt"));
             output.println(familie.toFile());
-    }
+    }*/
 
-    public static void loadKontaktPersonToDisk(ArrayList<Kontaktperson> listKP) throws FileNotFoundException {
+    /*public static void loadKontaktPersonToDisk(ArrayList<Kontaktperson> listKP) throws FileNotFoundException {
         PrintStream output = new PrintStream(new File("KontaktPersonData.txt"));
         for(int i = 0; i < listKP.size(); i++) {
             output.println(listKP.get(i).toFile());
         }
-    }
+    }*/
 
-    public static void loadBarnData()throws IOException {
+
+    /*public static void loadBarnData() throws IOException {
         BufferedReader buff = new BufferedReader(new FileReader("src/BarnData.txt"));
         ArrayList<String> read = new ArrayList<>();
-    }
-    public static ArrayList<Barn> loadBarnFromDisk() throws FileNotFoundException {
-        ArrayList<Barn> listB = new ArrayList<>();
         Scanner input = new Scanner(new File("src/BarnData.txt"));
         while(input.hasNext()) {
             String fornavn = input.next();
@@ -40,10 +38,9 @@ public class FileHandler {
             Barn temp = new Barn(fornavn, efternavn, cprNummer);
             listB.add(temp);
         }
-        return listB;
-    }
+    }*/
 
-    public static ArrayList<Kontaktperson> loadKontaktPersonFromDisk()throws FileNotFoundException {
+    /*public static ArrayList<Kontaktperson> loadKontaktPersonFromDisk()throws FileNotFoundException {
         ArrayList<Kontaktperson> listKP = new ArrayList<>();
         Scanner input = new Scanner(new File("src/kontaktpersonData.txt")).useDelimiter("|");
         while(input.hasNextLine()) {
@@ -51,15 +48,14 @@ public class FileHandler {
             String efternavn = input.next();
             String telefon = input.next();
             String email = input.next();
-            String addresse = input.next();
 
-            Kontaktperson temp = new Kontaktperson(fornavn, efternavn, telefon, email, addresse);
+            Kontaktperson temp = new Kontaktperson(fornavn, efternavn, telefon, email);
             listKP.add(temp);
         }
         return listKP;
-    }
+    }*/
 
-    public static void readFromMedarbejderData() throws IOException {
+    /*public static void readFromMedarbejderData() throws IOException {
         BufferedReader buff = new BufferedReader(new FileReader("src/MedarbejderData.txt"));
         ArrayList<String> read = new ArrayList<>();
         String line = buff.readLine();
@@ -68,10 +64,10 @@ public class FileHandler {
             read.add(line);
             line = buff.readLine();
         }
+        
+    }*/
 
-    }
-
-    public static ArrayList<Medarbejder> loadMedarbejderFromDisk()throws FileNotFoundException {
+    /*public static ArrayList<Medarbejder> loadMedarbejderFromDisk()throws FileNotFoundException {
         ArrayList<Medarbejder> listM = new ArrayList<>();
         Scanner input = new Scanner(new File("src/Medarbejderdata.txt")).useDelimiter("|");
         while(input.hasNextLine()){
@@ -83,12 +79,68 @@ public class FileHandler {
             Medarbejder temp = new Medarbejder(fornavn, efternavn, arbejdsdag, tidsrum, listM);
         }
         return listM;
-    }
+    }*/
 
-    public static void loadMedarbejderToDisk(ArrayList<Medarbejder> listM)throws FileNotFoundException {
+    /*public static void loadMedarbejderToDisk(ArrayList<Medarbejder> listM)throws FileNotFoundException {
         PrintStream output = new PrintStream(new File("MedarbejderData.txt"));
             for(int i = 0; i < listM.size(); i++) {
                 output.println(listM.get(i).toFile());
             }
+    }*/
+
+    public static void readFromFamilieData(ArrayList<Familie> listF, ArrayList<Barn> listB, ArrayList<Kontaktperson> listKP) throws IOException {
+        BufferedReader buff = new BufferedReader(new FileReader("src/FamilieData.txt"));
+        ArrayList<String> read = new ArrayList<>();
+        String line = buff.readLine();
+
+        while (line != null) {
+            read.add(line);
+            line = buff.readLine();
+        }
+
+        for (int i = 0; i < read.size(); i++) {
+            seperateChildrenFromAdults(read.get(i), listF, listB, listKP);
+        }
+    }
+
+    public static void seperateChildrenFromAdults(String text, ArrayList<Familie> listF, ArrayList<Barn> listB, ArrayList<Kontaktperson> listKP) {
+        String[] cut = text.split("/");
+        ArrayList<Kontaktperson> kontakt = new ArrayList<>();
+        System.out.println(cut[0]);
+        System.out.println(cut[1]);
+
+        Barn barn = barnToObject(cut[0]);
+        listB.add(barn);
+        String[] adult = cut[1].split("\"");
+        for (int i = 0; i < adult.length; i++) {
+            kontakt.add(kontaktToObject(adult[i]));
+        }
+        if (kontakt.size() == 1) {
+            Familie temp = new Familie(barn, kontakt.get(0));
+            listF.add(temp);
+        } else if (kontakt.size() == 2) {
+            Familie temp = new Familie(barn, kontakt.get(0), kontakt.get(1));
+            listF.add(temp);
+        } else if (kontakt.size() == 3) {
+            Familie temp = new Familie(barn, kontakt.get(0), kontakt.get(1), kontakt.get(2));
+            listF.add(temp);
+        }
+
+    }
+
+    public static Barn barnToObject(String text) {
+        String[] barn1 = text.split(",");
+
+        Barn temp = new Barn(barn1[0], barn1[1], barn1[2]);
+        System.out.println(temp);
+            return temp;
+    }
+
+    public static Kontaktperson kontaktToObject(String text) {
+        String[] kontakt = text.split(",");
+
+        Kontaktperson temp = new Kontaktperson(kontakt[0], kontakt[1], kontakt[2], kontakt[3]);
+        System.out.println(temp);
+            return temp;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args)throws FileNotFoundException {
-        Familie.opretFamilie();
-        /*ArrayList<Medarbejder> listM = FileHandler.loadMedarbejderFromDisk();
-        ArrayList<Barn> listB = FileHandler.loadBarnFromDisk();
-        ArrayList<Kontaktperson> listKP = FileHandler.loadKontaktPersonFromDisk();
+    public static void main(String[] args) throws IOException {
         ArrayList<Familie> listF = new ArrayList<>();
+        ArrayList<Barn> listB = new ArrayList<>();
+        ArrayList<Kontaktperson> listKP = new ArrayList<>();
+        FileHandler.readFromFamilieData(listF, listB, listKP);
+        Familie.opretFamilie();
+        //ArrayList<Medarbejder> listM = FileHandler.loadMedarbejderFromDisk();
+        //ArrayList<Barn> listB = FileHandler.loadBarnFromDisk();
+        //ArrayList<Kontaktperson> listKP = FileHandler.loadKontaktPersonFromDisk();
         //Medarbejder.menuMedarbejderVagtplan(listM);
-        System.out.println("hello");
-        Menu.menuSelection(listF, listB, listM, listKP);
+        Menu.menuSelection(listF, listB, listKP);
         Kontaktperson test = Kontaktperson.skabNyKontaktperson();
-        System.out.println(test);*/
+        System.out.println(test);
+
     }
 }
